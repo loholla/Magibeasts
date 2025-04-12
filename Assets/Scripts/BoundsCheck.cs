0f33a3afecae3ad6869867ac26dc189aac82f5f6
Shadow-Diamond <147402627+loholla@users.noreply.github.com>
2025-04-12 15:32:00 -0500

    bounds check added

---
+++ Assets/Scripts/BoundsCheck.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public float xBound;
+    public float zBound;
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > xBound){
+            pos.x -= 0.1f;
+            print("Off Right");
+        }
+        if(pos.x < -xBound){
+            pos.x += 0.1f;
+            print("Off Left");
+        }
+        if(pos.z > zBound){
+            pos.z -= 0.1f;
+            print("Off Top");
+        }
+        if(pos.z < -zBound){
+            pos.z += 0.1f;
+            print("Off Bottom");
+        }
+
+        transform.position = pos;
+    }
+}
